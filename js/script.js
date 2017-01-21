b0c6f128a114e15c6c443e016cc3ac206847042d
Serge Huber <serghubr@gmail.com>
2017-01-21 17:25:23 +0200

    Translate into English and make some improvements in the code

--- js/script.js
+++ js/script.js
@@ -1,78 +1,72 @@
 function getCurrentDate() {
-  var today = new Date();
-  var day = today.getDate();
-  var month = today.getMonth() + 1;
-  var year = today.getFullYear();
+  var date = new Date();
+  var day = date.getDate();
+  var month = date.getMonth() + 1;
+  var year = date.getFullYear();
 
-  if(day<10) {
-    day='0'+day;
-  }
+  if (day < 10) day = '0' + day;
+  if (month < 10) month = '0' + month;
 
-  if(month<10) {
-    month='0'+month;
-  }
-
-  return today = day + '.' + month + '.' + year;
-}
-
-function isSelectEmpty() {
-  var $selectList = $('select');
-  for(var i=0; i < $selectList.length; i++) {
-    if($selectList[i].value == "") return true;
-  }
-  return false;
-}
-
-function getDate(n) {
-  var $selectList = $('select');
-  return $selectList[n].value;
+  return day + '.' + month + '.' + year;
 }
 
 function daysCounter() {
-  var date1 = new Date(getDate(2), getDate(1), getDate(0));
-  var date2 = new Date(getDate(5), getDate(4), getDate(3));
-  var timeDiff = Math.abs(date2.getTime() - date1.getTime());
-  var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-  if($("#endDay").prop('checked')) diffDays++;
-  return diffDays;
+  var timeDifference = Math.abs(date2.getTime() - date1.getTime());
+  var daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+
+  if ($("#endDay").prop('checked')) daysDifference++;
+
+  return daysDifference;
 }
 
 function weeksCounter() {
-  var daysCol = daysCounter();
-  var weeks = daysCol/7;
-  if(weeks < 1) return 0;
-  return Math.floor(weeks);
+  var daysDifference = daysCounter();
+  var weeksDifference = daysDifference / 7;
+
+  return (weeksDifference < 1) ?  0 : Math.floor(weeksDifference);
 }
 
 function monthsCounter() {
-  var months;
-  var d1 = new Date(getDate(2), getDate(1), getDate(0));
-  var d2 = new Date(getDate(5), getDate(4), getDate(3));
-  months = (d2.getFullYear() - d1.getFullYear()) * 12;
-  months -= d1.getMonth() + 1;
-  months += d2.getMonth() + 1;
-  if(months < 0) return -months;
-  return months;
+  var monthsDifference = (date2.getFullYear() - date1.getFullYear()) * 12;
+
+  monthsDifference -= date1.getMonth() + 1;
+  monthsDifference += date2.getMonth() + 1;
+
+  return (monthsDifference < 0) ? -monthsDifference : monthsDifference;
 }
 
 function yearsCounter() {
-  var d1 = new Date(getDate(2), getDate(1), getDate(0));
-  var d2 = new Date(getDate(5), getDate(4), getDate(3));
-  var years = (d2.getFullYear() - d1.getFullYear());
-  if(years < 0) return -years;
-  return years;
+  var yearsDifference = (date2.getFullYear() - date1.getFullYear());
+  return (yearsDifference < 0) ? -yearsDifference : yearsDifference;
 }
 
-$('.alert').html('Сегодня ' + getCurrentDate());
-
 $('#submit').click(function() {
-  if(isSelectEmpty()) {
-    $('#errorModal').modal('show');
-    return;
+  var selectList = $('select');
+
+  for (var i = 0; i < selectList.length; i++) {
+    if (selectList[i].value == "") {
+      $('#modal-2').modal('show');
+      return;
+    }
   }
-  $('.daysPassed').html(daysCounter());
-  $('.weeksPassed').html(weeksCounter());
-  $('.monthsPassed').html(monthsCounter());
-  $('.yearsPassed').html(yearsCounter());
+
+  window.date1 = new Date(selectList[2].value, selectList[1].value, selectList[0].value);
+  window.date2 = new Date(selectList[5].value, selectList[4].value, selectList[3].value);
+
+  $('.days-between').html(daysCounter());
+  $('.weeks-between').html(weeksCounter());
+  $('.months-between').html(monthsCounter());
+  $('.years-between').html(yearsCounter());
+
   $('#modal-1').modal('show');
 });
+
+$('.current-date span').html('Today is ' + getCurrentDate());
+
+for (var i = 1; i <= 31; i++) {
+  $('.days').append('<option value="'+i+'">'+i+'</option>');
+}
+
+for (var i = 1900; i <= 2100; i++) {
+  $('.year').append('<option value="'+i+'">'+i+'</option>');
+}
